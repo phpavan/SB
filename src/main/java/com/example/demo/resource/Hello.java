3019d150cd4de795f37940e4ed82d85671366b31
phpavan <phpavan@gmail.com>
2020-10-22 22:13:13 +0800

    Add files via upload

---
+++ src/main/java/com/example/demo/resource/Hello.java
@@ -0,0 +1,17 @@
+package com.example.demo.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class Hello {
+
+	@GetMapping
+  public String hello(){
+    return "Hello UTube";
+  }
+
+}
